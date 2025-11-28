f38f31d8f4a322bc1141eb08a0436e32faa33395
WAndrea0207 <firexdarkx@gmail.com>
2025-11-28 19:27:08 +0100

    dropdown changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,17 @@ export default function About() {
             <Banner image={bannerAbout}/>
             <div className="about__dropdowns">
                 <Dropdown title="Fiabilité">
+                    Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
                 </Dropdown>
                 <Dropdown title="Respect">
+                    La bienveillance fait partie des valeurs fondatrices de Kasa. 
+                    Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme
                 </Dropdown>
                 <Dropdown title="Service">
+                    La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.
                 </Dropdown>
                 <Dropdown title="Sécurité">
+                    La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
                 </Dropdown>
             </div>
             
